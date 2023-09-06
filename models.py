62e03112b97277d5b61c02354b56e8e412c658c6
ian otieno <ianotieno23@gmail.com>
2023-09-06 12:48:55 +0300

    update

--- models.py
+++ models.py
@@ -1,6 +1,14 @@
-from sqlalchemy import Column
+from datetime import datetime
+from sqlalchemy import Column, DateTime, Integer, String
 from sqlalchemy.orm import declarative_base
 Base =declarative_base()
 
 class UserModel(Base):
+    __tablename__="user"
+    
     id=Column(Integer,primary_key=True)
+    first_name=Column(String,nullable=False)
+    last_name=Column(String,nullable=False)
+    birth= Column(DateTime)
+    created =Column(DateTime,default=datetime.utcnow)
+    
